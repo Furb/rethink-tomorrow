41d42736cbf82285bb900186d9a682ee5b4e289d
Jesper <jthabo@gmail.com>
2025-03-27 07:35:44 +0100

    added container + satoshi font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <div className="grid grid-cols-3">
+        <div className="h-8 w-full bg-dark col-span-full wrap"></div>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+      </div>
+    </main>
+  );
 }
